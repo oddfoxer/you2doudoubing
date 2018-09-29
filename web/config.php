328e4542eee9e5f28e27d1c469176ea26d320c63
oddfoxer <43650204+oddfoxer@users.noreply.github.com>
2018-09-29 18:18:34 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,8 +2,8 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyAurg5k-lp9qQxA_IpC5x48YZ3-cslEQ5Q');
 define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('SITE_NAME', 'FoxTV');
+define('TITLENAME', '首页 WeChat: odd-fox');
 define('EN2DEKEY', 'lujianbupingyishenghou');
 define('EMAIL', 'xiaofufua@protonmail.com');
 ?>
